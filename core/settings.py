f93216c0066ff4b37d2c5350ac8ea3d0696f23d7
abdo <abdo.bougaa@gmail.com>
2022-06-05 10:59:30 +0100

    add auth

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
     'rest_framework',
+    'dj_rest_auth',
     'payment'
 ]
 
--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('dj_rest_auth.urls')),
     path('payment/', include('payment.urls')),
-    path('testing/', admin.site.urls),
 ]
